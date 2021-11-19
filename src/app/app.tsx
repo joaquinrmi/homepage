9cf85f9242be8da3c627c97f96a7c791935f052c
joaquinrmi <joaquinruaimi@gmail.com>
2021-11-19 20:28:43 -0300

    Update app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Route, HashRouter as Router } from "react-router-dom";
+import { Routes } from "react-router";
+
+import Home from "../pages/home/";
 
 import "./app.scss";
 
 const App: React.FunctionComponent = (props) =>
 {
-    return <>
-        ¡Hola mundo!
-    </>;
+    return <Router>
+        <Routes>
+            <Route path="/*" element={ <Home /> } />
+        </Routes>
+    </Router>;
 };
 
 export default App;
