94369356138d6e9a6d68bae5e482c61d3735bcf7
joaquinrmi <joaquinruaimi@gmail.com>
2021-11-19 20:28:20 -0300

    Add routes to Home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Routes, Route } from "react-router";
 
 import Button, { Style } from "../../components/button/";
 import SignupForm from "../../components/signup_form/";
+import LoginForm from "../../components/login_form/";
 
 import GitHubLogo from "../../res/GitHub-Mark-Light-32px.png";
 
@@ -13,42 +15,66 @@ export interface Props
 const Home: React.FunctionComponent<Props> = (props) =>
 {
     return <div className="home">
-        <div className="title-container">
-            <div className="content-wrapper">
-                <div className="message-container">
-                    <span className="title">
-                        ¡Bienvenid@!
-                    </span>
+        <Routes>
+            <Route path="/login" element={<>
+                <div className="form-section single">
+                    <div className="content-wrapper">
+                        <div className="form-container">
+                            <LoginForm id="main-login" />
+                        </div>
+                    </div>
+                </div>
+            </>} />
 
-                    <span className="subtitle">
-                        Soy una simple página de inicio.
-                    </span>
+            <Route path="/signup" element={<>
+                <div className="form-section single">
+                    <div className="content-wrapper">
+                        <div className="form-container">
+                            <SignupForm id="main-signup" />
+                        </div>
+                    </div>
+                </div>
+            </>} />
+
+            <Route path="/*" element={<>
+                <div className="title-container">
+                    <div className="content-wrapper">
+                        <div className="message-container">
+                            <span className="title">
+                                ¡Bienvenid@!
+                            </span>
+
+                            <span className="subtitle">
+                                Soy una simple página de inicio.
+                            </span>
+                        </div>
+
+                        <div className="button-container">
+                            <a className="home-link" href="https://github.com/joaquinrmi/homepage" target="_blank">
+                                <Button
+                                    id="github-button"
+                                    style={Style.Transparent}
+                                    message={
+                                        <>REPOSITORIO</>
+                                    }
+                                    iconImage={
+                                        <img src={GitHubLogo} />
+                                    }
+                                />
+                            </a>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="button-container">
-                    <a className="home-link" href="https://github.com/joaquinrmi/homepage" target="_blank">
-                        <Button
-                            id="github-button"
-                            style={Style.Transparent}
-                            message={
-                                <>REPOSITORIO</>
-                            }
-                            iconImage={
-                                <img src={GitHubLogo} />
-                            }
-                        />
-                    </a>
+                <div className="form-section">
+                    <div className="content-wrapper">
+                        <div className="form-container">
+                            <SignupForm id="home-signup" />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-
-        <div className="form-container">
-            <div className="content-wrapper">
-                <div className="signup-form-container">
-                    <SignupForm id="home-signup" />
-                </div>
-            </div>
-        </div>
+            </>} />
+        </Routes>
     </div>;
 };
 
